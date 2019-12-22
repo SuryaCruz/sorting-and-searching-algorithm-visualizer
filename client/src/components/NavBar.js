5f9034769c6210a3dfe3c9f012bf2586837880fb
Surya Cruz <spectrablitz5001@gmail.com>
2019-12-22 23:40:28 +0100

    UPDATED: Navbar v2. TODO: Documentation. Redux.

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,16 +4,52 @@ import {
     InputGroup,
     InputGroupText,
     InputGroupAddon,
-    Button
+    Button,
+    ButtonGroup
 } from 'reactstrap'
 
 class NavBar extends Component{
     constructor(props){
         super(props);
+        this.state = {search: true};
+        this.setSearch = this.setSearch.bind(this);
+        this.setSort = this.setSort.bind(this);
     }
 
+    setSearch(){
+        this.setState({search:true});
+    }
+
+    setSort(){
+        this.setState({search:false});
+    }
 
     render(){
+        var show = this.state.search;
+        const search = (
+            <div className = "algorithms">
+                <Button color = "info" size = "mb" className = "buttonPadding"> Binary Search </Button>
+                <Button color = "info" size = "mb" className = "buttonPadding"> Linear Search </Button>
+            </div>
+        );
+
+        const sort = (
+            <div className = "algorithms">
+                <Button color = "info" size = "mb" className = "buttonPadding"> Selection Sort </Button>
+                <Button color = "info" size = "mb" className = "buttonPadding"> Bubble Sort </Button>
+            </div>
+        );
+        
+        var alg = search;
+
+        if(show){
+            alg = search;
+        }else{
+            alg = sort;
+        }
+
+
+
         return(
             <div className="navBar">
                 <div className = "arrayInput">
@@ -22,12 +58,12 @@ class NavBar extends Component{
                         <Input placeholder = "eg: [ 1 , 2 , 3 ]" size = "sm"/>
                     </InputGroup>
                 </div>
-                <div className = "algorithms">
-                    <Button color = "info" size = "mb" className = "buttonPadding"> Binary Search </Button>
-                    <Button color = "info" size = "mb" className = "buttonPadding"> Linear Search </Button>
-                    <Button color = "info" size = "mb" className = "buttonPadding"> Selection Sort </Button>
-                    <Button color = "info" size = "mb" className = "buttonPadding"> Bubble Sort </Button>
-                </div>
+                <ButtonGroup className = "buttons">
+                            <Button color="success" onClick={this.setSearch} >Searching</Button>
+                            <Button color="danger" onClick={this.setSort} >Sorting</Button>
+                </ButtonGroup>
+                {alg}
+                
             </div>
         )
 
