9bb9046b7044d939ed41626f49ccaa7874b410e3
Surya Cruz <spectrablitz5001@gmail.com>
2019-12-25 02:50:55 +0100

    ADDED: Error checking for array input.

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -11,27 +11,49 @@ import {
 class NavBar extends Component{
     constructor(props){
         super(props);
-        this.state = {search: true,array:[]};
+        this.state = {search: true,array:[],check: false};
         this.setSearch = this.setSearch.bind(this);
         this.setSort = this.setSort.bind(this);
         this.changeArray = this.changeArray.bind(this);
     }
 
     setSearch(){
-        this.setState({search:true, array: this.state.array});
+        this.setState({search:true, array: this.state.array, check: this.state.check});
     }
 
     setSort(){
-        this.setState({search:false, array: this.state.array});
+        this.setState({search:false, array: this.state.array, check: this.state.check});
     }
 
     changeArray(e){
-        this.setState({search:this.state.search, array: e.target.value});
+        var invalid = false;
+        var input = e.target.value.split("");
+        var stri = "";
+        for (var i of input){
+            if(!(i === '[' || i === ']' || i === ' ' || i === ',' || (i >= '0' && i <= '9'))){
+                invalid = true;
+                numbers = [];
+                break;
+            }else{
+                if(!(i === '[' || i === ']' || i === ' ')){
+                    stri = stri + i;
+                }
+                var numbers = stri.split(",");
+                if(numbers[numbers.length-1] === ""){
+                    numbers.pop();
+                }
+                for (var c = 0; c < numbers.length; c++){
+                    numbers[c] = Number(numbers[c]);
+                }   
+            }
+            
+        }
+        console.log(numbers);
+        this.setState({search:this.state.search, array: numbers,check: invalid});
     }
     
 
     render(){
-
         var input = this.state.array;
         var show = this.state.search;
         const search = (
@@ -56,12 +78,14 @@ class NavBar extends Component{
             alg = sort;
         }
 
+
+
         return(
             <div className="navBar">
                 <div className = "arrayInput">
                     <InputGroup>
                         <InputGroupAddon addonType = "prepend"><Button size = "sm" color = "primary">Random</Button></InputGroupAddon>
-                        <Input placeholder = "eg: [ 1 , 2 , 3 ]" size = "sm" onChange = {this.changeArray}/>
+                        <Input invalid = {this.state.check} placeholder = "eg: [ 1 , 2 , 3 ]" size = "sm" onChange = {this.changeArray}/>
                     </InputGroup>
                 </div>
                 <ButtonGroup className = "buttons">
